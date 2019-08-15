0c1dd24932e7edd1486f87d436236a74df507235
Максим <firoks1@gmail.com>
2019-08-15 16:00:16 +0300

    Кривой перенос вещей

--- js/dialog.js
+++ js/dialog.js
@@ -43,6 +43,57 @@ var onUploadElementMouseDown = function (evtMouseDown) {
   document.addEventListener('mouseup', onDocumentMouseUp);
 };
 
+var onArtifactsShopElementMouseDown = function (evtMouseDown) {
+  evtMouseDown.preventDefault();
+
+  var element = evtMouseDown.target;
+
+  element.style.position = 'absolute';
+
+  var startCoordinates = {
+    x: evtMouseDown.clientX,
+    y: evtMouseDown.clientY
+  };
+
+  var onDocumentMouseMove = function (evtMouseMove) {
+    evtMouseMove.preventDefault();
+
+    var shift = {
+      x: startCoordinates.x - evtMouseMove.clientX,
+      y: startCoordinates.y - evtMouseMove.clientY
+    };
+
+    startCoordinates = {
+      x: evtMouseDown.clientX,
+      y: evtMouseDown.clientY
+    };
+
+    element.style.left = (startCoordinates.x - shift.x) + 'px';
+    element.style.top = (startCoordinates.y - shift.y) + 'px';
+  };
+
+  var onUserArtifactsElementMouseOver = function (evtMouseOver) {
+    evtMouseOver.preventDefault();
+
+    evtMouseOver.target.appendChild(element);
+    element.removeAttribute('style');
+
+    userArtifactsElement.removeEventListener('mouseover', onUserArtifactsElementMouseOver);
+  };
+
+  var onDocumentMouseUp = function (evtMouseUp) {
+    evtMouseUp.preventDefault();
+
+    document.removeEventListener('mousemove', onDocumentMouseMove);
+    document.removeEventListener('mouseup', onDocumentMouseUp);
+  };
+
+  userArtifactsElement.addEventListener('mouseover', onUserArtifactsElementMouseOver);
+
+  document.addEventListener('mousemove', onDocumentMouseMove);
+  document.addEventListener('mouseup', onDocumentMouseUp);
+};
+
 var onUploadElementClick = function (evt) {
   evt.preventDefault();
 
@@ -52,7 +103,15 @@ var onUploadElementClick = function (evt) {
 var setupElement = document.querySelector('.setup');
 var uploadElement = setupElement.querySelector('.upload');
 
+var artifactsShopElement = setupElement.querySelector('.setup-artifacts-shop');
+var artifactsShopCellElements = artifactsShopElement.querySelectorAll('.setup-artifacts-cell');
+
+var userArtifactsElement = setupElement.querySelector('.setup-artifacts');
+var userArtifactsCellElements = userArtifactsElement.querySelectorAll('.setup-artifacts-cell');
+
 uploadElement.addEventListener('mousedown', onUploadElementMouseDown);
+artifactsShopElement.addEventListener('mousedown', onArtifactsShopElementMouseDown);
+
 
 window.dialog = {
   setDefaultCoordinates: function () {
